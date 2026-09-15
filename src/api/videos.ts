190e79fcff99e7aaed9df95918cd982785cc9575
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-09-15 17:59:05 -0400

    AWS s3 intro section. Mainly video upload function

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -2,7 +2,52 @@ import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { BadRequestError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const MAX_UPLOAD_SIZE = 1 << 30; //1GB
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  console.log("uploading video", videoId, "by user", userID);
+
+  const video = getVideo(cfg.db, videoId);
+  if (video?.userID !== userID) {
+    throw new UserForbiddenError("You do not own this video");
+  }
+
+  const formData = await req.formData();
+  const videoData = formData.get("video");
+  if (!(videoData instanceof File)) {
+    throw new BadRequestError("Video is not type File");
+  }
+  if (videoData.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Video is too large. Max upload size is 1GB");
+  }
+
+  const mediaType = videoData.type;
+  if (mediaType !== "video/mp4") {
+    throw new BadRequestError("Video must be an mp4");
+  }
+
+  const tempPath = `${videoId}.mp4`;
+  await Bun.write(tempPath, videoData);
+
+  const file = cfg.s3Client.file(tempPath);
+  file.write(Bun.file(tempPath), {
+    type: mediaType,
+  });
+
+  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoId}.mp4`;
+  updateVideo(cfg.db, video);
+
+  await Bun.file(tempPath).delete();
   return respondWithJSON(200, null);
 }
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { newDatabase } from "./db/db";
+import { s3, S3Client } from "bun";
 import type { Database } from "bun:sqlite";
 
 export type ApiConfig = {
@@ -10,6 +11,7 @@ export type ApiConfig = {
   s3Bucket: string;
   s3Region: string;
   s3CfDistribution: string;
+  s3Client: S3Client;
   port: string;
 };
 
@@ -34,6 +36,7 @@ export const cfg: ApiConfig = {
   s3Bucket: s3Bucket,
   s3Region: s3Region,
   s3CfDistribution: s3CfDistribution,
+  s3Client: s3,
   port: port,
 };
 
